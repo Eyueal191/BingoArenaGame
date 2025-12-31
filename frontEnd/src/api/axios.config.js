db44a6cebcf12a30fd23436b1afb2b1bf3c2fd5a
Eyueal191 <eyuealayalew191@gmail.com>
2025-12-31 00:11:35 -0800

    backend url set up

--- frontEnd/src/api/axios.config.js
+++ frontEnd/src/api/axios.config.js
@@ -4,7 +4,7 @@ import axios from "axios";
  * Axios instance
  */
 const Axios = axios.create({
-  baseURL:"https://bingoarenagame-2.onrender.com",
+  baseURL:"https://bingo-fn13.onrender.com",
   timeout: 10000, // 10 seconds
   headers: {
     "Content-Type": "application/json",
